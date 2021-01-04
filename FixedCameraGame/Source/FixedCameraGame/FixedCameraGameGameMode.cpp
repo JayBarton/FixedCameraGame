7bc90929917ac933898d5213cb1ba39d5b3e1d5a
JayBarton <damonjaybarton@gmail.com>
2021-01-04 18:06:20 -0500

    More work on the inventory tutorial

--- FixedCameraGame/Source/FixedCameraGame/FixedCameraGameGameMode.cpp
+++ FixedCameraGame/Source/FixedCameraGame/FixedCameraGameGameMode.cpp
@@ -11,7 +11,7 @@
 AFixedCameraGameGameMode::AFixedCameraGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_FixedCameraPlayer"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BPFCPlayer"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
